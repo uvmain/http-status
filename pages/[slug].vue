6a2848d369daa4a4ff091b5af56e9073c26cda75
Ian Baron <ian.baron@manchester.ac.uk>
2024-04-16 12:45:09 +0100

    init

---
+++ pages/[slug].vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { getReasonPhrase } from 'http-status-codes';
+
+const route = useRoute()
+const statusCode = Number(`${route.params.slug}`)
+const statusReason = getReasonPhrase(statusCode)
+
+if (statusCode < 200 || statusCode > 299) {
+  throw createError({
+  statusCode: statusCode,
+  statusMessage: statusReason
+})
+}
+</script>
+
+<template>
+  <div>
+    <h2>{{ statusCode }}</h2>
+  </div>
+</template>
