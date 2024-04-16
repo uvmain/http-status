f8d0b4ad8615b6ce46ed64118482f9482dcb011d
Ian Baron <ian.baron@manchester.ac.uk>
2024-04-16 18:29:20 +0100

    return 200-300status codes

--- error.vue
+++ error.vue
@@ -8,7 +8,7 @@ const props = defineProps({
 
 <template>
   <div>
-    <h1>{{ error.statusCode }}</h1>
+    <h1>{{ error?.statusCode }} {{  error?.statusMessage }}</h1>
     <NuxtLink to="/">Go back home</NuxtLink>
   </div>
 </template>
--- pages/[slug].vue
+++ pages/[slug].vue
@@ -6,16 +6,14 @@ const route = useRoute()
 const statusCode = Number(`${route.params.slug}`)
 const statusReason = getReasonPhrase(statusCode)
 
-if (statusCode < 200 || statusCode > 299) {
-  throw createError({
+throw createError({
   statusCode: statusCode,
   statusMessage: statusReason
 })
-}
 </script>
 
 <template>
   <div>
-    <h2>{{ statusCode }}</h2>
+    <h2>{{ statusCode }}: {{ statusReason }}</h2>
   </div>
 </template>
